55681409ad0eb94abf5ea737c016bca11d33eeeb
Bobby <bodavis@csumb.edu>
2022-02-24 09:09:32 -0800

    add properties to Product model

--- src/main/java/com/revature/models/Product.java
+++ src/main/java/com/revature/models/Product.java
@@ -1,4 +1,19 @@
 package com.revature.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Product {
+    @Id
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="fk_shop_product_id")
+    private int fkShopProductId;
+
+    private String name;
+    private String description;
 }
