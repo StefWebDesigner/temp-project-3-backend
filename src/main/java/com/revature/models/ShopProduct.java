51bf3fbf91670d275ffaa17268cdb628a42559b9
Bobby <bodavis@csumb.edu>
2022-02-24 09:14:18 -0800

    Add properties to ShopProduct model

--- src/main/java/com/revature/models/ShopProduct.java
+++ src/main/java/com/revature/models/ShopProduct.java
@@ -1,4 +1,18 @@
 package com.revature.models;
 
+import javax.persistence.*;
+
+@Entity
 public class ShopProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="fk_shop_id")
+    private int fkShopId;
+
+    private int quantity;
+    private double price;
+    private double discount;
 }
