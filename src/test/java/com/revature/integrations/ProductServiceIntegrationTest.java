dca076ce66ada479558ad1cfd7d3a123c6a0ef5c
Bobby <bodavis@csumb.edu>
2022-02-25 15:35:44 -0800

    Add missing ProductService import

--- src/test/java/com/revature/integrations/ProductServiceIntegrationTest.java
+++ src/test/java/com/revature/integrations/ProductServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.revature.integrations;
 
 import com.revature.models.Product;
+import com.revature.services.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
