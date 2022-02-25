8521dbc850e5f14e21b6a6d20a73fd01121f51b8
benkarlsberg <benelliot16@gmail.com>
2022-02-25 07:44:05 -0800

    add category service test getCategoryById()

--- src/test/java/com/revature/controllers/CategoryControllersTests.java
+++ src/test/java/com/revature/controllers/CategoryControllersTests.java
@@ -1,4 +1,40 @@
 package com.revature.controllers;
 
+import com.revature.models.Category;
+import com.revature.models.Product;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 public class CategoryControllersTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @Test
+    public void testGetCategoryById() throws Exception {
+        Mockito.when(ps.getAllCategories().thenReturn(new Category(1, "Food")));
+
+        ResultActions ra = mvc.perform(MockMvcRequestBuilders.get("/products/category/1"));
+        ra.andExpect(MockMvcResultMatchers.status().isOk());
+
+        Mockito.when(ps.getProductById(3).thenReturn(null));
+        ra = mvc.perform(MockMvcRequestBuilders.get("/products/category/3"));
+        ra.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 }
