1d8e31dc298ac057fb9fd96c5ba482eef2391d9f
Bobby <bodavis@csumb.edu>
2022-02-26 11:13:10 -0800

    Make mock Product static

--- src/test/java/com/revature/controllers/ProductControllersTests.java
+++ src/test/java/com/revature/controllers/ProductControllersTests.java
@@ -1,8 +1,10 @@
 package com.revature.controllers;
 
 import com.revature.dartcart.DartCartApplication;
+import com.revature.models.Category;
 import com.revature.models.Product;
 import com.revature.services.ProductService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 //import org.junit.runner.RunWith;
@@ -23,6 +25,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 @AutoConfigureMockMvc
@@ -42,12 +46,19 @@ public class ProductControllersTests {
         mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
+    final static Product testProduct = new Product(
+            1,"testProduct",
+            "testDescription",
+            new ArrayList<Category>(Arrays.asList(new Category(1, "testCategory")))
+    );
+
+//    Product testProduct = new Product();
+//    testProduct.setId(1);
+//    testProduct.setName("testProduct");
+//    testProduct.setDescription("testDescription");
+
     @Test
-    public void testGetProductById() throws Exception {
-        Product testProduct = new Product();
-        testProduct.setId(1);
-        testProduct.setName("testProduct");
-        testProduct.setDescription("testDescription");
+    public void testGetProductByIdSuccess() throws Exception {
         Mockito.when(ps.getProductById(1)).thenReturn(Optional.of(testProduct));
 
         ResultActions ra = mvc.perform(MockMvcRequestBuilders.get("/products/1"));
