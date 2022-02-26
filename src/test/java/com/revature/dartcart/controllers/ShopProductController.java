a3628f476ed23667b4fa9964046c592d653ebea7
Tomas Anbessaw <tomasanbessaw@gmail.com>
2022-02-26 09:07:28 -0800

    tommy's changes

---
+++ src/test/java/com/revature/dartcart/controllers/ShopProductController.java
@@ -0,0 +1,38 @@
+package com.revature.dartcart.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Id;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class ShopProductController {
+    @Autowired
+    private ShopProductService sps;
+
+    @GetMapping("/shopproducts/{id}")
+    public ResponseEntity<ShopProduct> getShopProductByShopId(@PathVariable ("id") String id) {
+        ShopProduct sp = sps.getShopProductByShopId(Integer.parseInt(id));
+
+        if(sp != null) {
+          return new ResponseEntity<ShopProduct>(sp, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<ShopProduct>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/shopproducts")
+    public List<ShopProduct> getAllShopProducts() {
+        return sps.getAllShopProducts;
+    }
+
+
+}
---
+++ src/test/java/com/revature/dartcart/repositories/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.revature.dartcart.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+    public interface ProductRepo extends CrudRepository<Product, Integer> {
+
+}
