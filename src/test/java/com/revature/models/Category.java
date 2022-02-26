3069252c488e84c0db484f3cc91eb7654fa20d57
Jack <iatenine@gmail.com>
2022-02-26 12:55:05 -0700

    Standardized and Consolidated Models
    Created a standardized models package, consolidated the Customer and User tables together, applied naming conventions and
    fixed an inaccurate ID from the old methods thanks to contributions by Robert Davis. Branch is based off of dev as to ensure
    minimal conflicts and package structure was designed to reduce merge conflicts. Most developers should be able to merge this
    branch into theirs with no conflicts so long as they first delete any models package they already have

---
+++ src/test/java/com/revature/models/Category.java
@@ -0,0 +1,26 @@
+
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * This class represents an individual Category a Product may belong to.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int id;
+
+    private String name;
+}
---
+++ src/test/java/com/revature/models/User.java
@@ -0,0 +1,55 @@
+
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * This class represents a User entity in the database.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    private long registrationDate;
+
+    private String homePage;
+
+    @Column(length = 1000)
+    private String location;
+
+    @Column(length = 1000)
+    private String description;
+
+    // Returns items in both cart and wishlist
+    // Filter by CartItem's saved field to separate the lists
+    @OneToMany
+    @JoinColumn(name = "cart_item_id")
+    private List<CartItem> itemList;
+
+}
