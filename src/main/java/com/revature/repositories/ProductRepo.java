4e3a94b8b95e834134047f45220493001dae2091
Tomas Anbessaw <tomasanbessaw@gmail.com>
2022-02-28 12:39:10 -0800

    fixed repo error

--- src/main/java/com/revature/repositories/ProductRepo.java
+++ src/main/java/com/revature/repositories/ProductRepo.java
@@ -1,6 +1,7 @@
 package com.revature.repositories;
 
 
+import com.revature.models.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
