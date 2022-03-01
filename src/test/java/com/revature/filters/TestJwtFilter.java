f6917793431d70a75111128e6d94722bcd449a47
Anthony-Davis <adavi00@outlook.com>
2022-03-01 10:33:29 -0500

    JwtTokenFilter
    jwtFilter tests updated.
    implementation added in utilities/JwtTokenFilter

--- src/test/java/com/revature/filters/TestJwtFilter.java
+++ src/test/java/com/revature/filters/TestJwtFilter.java
@@ -1,6 +1,8 @@
 package com.revature.filters;
 
+import com.revature.app.DartCartApplication;
 import com.revature.models.User;
+import com.revature.repositories.UserRepo;
 import com.revature.utilities.JwtTokenUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -15,44 +18,44 @@ import org.springframework.mock.web.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = DartCartApplication.class)
 public class TestJwtFilter {
 	
-	// inject jwt token util class and user repo before
-	// jwtTokenUtil,  getUsername
-	// CHECKS: token valid, username, is authenticated
-	// assertTrue( SecurityContextHolder.getContext().getAuthentication().isAuthenticated() );
-	
-	private static final String CLASSNAME = "com.revature.dartcart.filters.JwtTokenFilter";
+	private static final String CLASSNAME = "com.revature.utilities.JwtTokenFilter";
 	private static final String DOFILTER = "doFilterInternal";
 	private static Object tokenFilter;
 	private static Method doFilterInternal;
 	
 	@MockBean private static JwtTokenUtil jwtTokenUtil;
-	@MockBean private static Class UserRepo;
+	@MockBean private static UserRepo userRepo;
 	@Autowired private MockMvc mvc;
 	
-	//@Autowired MockHttpServletRequest request;
-	
+	// @Autowired ApplicationContext app;
+	// @Autowired MockHttpServletRequest request;
 	
 	@BeforeAll
 	static void testSetup(@Autowired ApplicationContext app) {
-		
 		try {
 			tokenFilter = app.getBean(Class.forName(CLASSNAME));
-			doFilterInternal = tokenFilter.getClass().getMethod(DOFILTER);
+			
+			doFilterInternal = tokenFilter.getClass().getDeclaredMethod(DOFILTER, HttpServletRequest.class, HttpServletResponse.class, FilterChain.class);
 		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Class: " + CLASSNAME+ " needs to be implemented", e);
+			throw new RuntimeException("Class: " + CLASSNAME + " needs to be implemented", e);
 		} catch (NoSuchMethodException e) {
-			throw new RuntimeException("Method: " + DOFILTER+ " needs to be implemented", e);
+			throw new RuntimeException("Method: " + DOFILTER + " needs to be implemented", e);
 		}
 	}
 	
 	@Test
+	//@ParameterizedTest
 	void tokenDNE() throws InvocationTargetException, IllegalAccessException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		MockHttpServletResponse response = new MockHttpServletResponse();
@@ -87,7 +90,7 @@ public class TestJwtFilter {
 		Mockito.when(jwtTokenUtil.validate("exampleToken")).thenReturn(true);
 		
 		Mockito.when( jwtTokenUtil.getUsername("exampleToken")).thenReturn("invalidName");
-		Mockito.when(UserRepo.findByUsername("invalidName") ).thenReturn(null);
+		Mockito.when(userRepo.findByUsername("invalidName") ).thenReturn(null);
 		
 		doFilterInternal.invoke(tokenFilter, request, response, mockChain);
 		assertFalse(SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
@@ -103,7 +106,7 @@ public class TestJwtFilter {
 		Mockito.when(jwtTokenUtil.validate("exampleToken")).thenReturn(true);
 		
 		Mockito.when( jwtTokenUtil.getUsername("exampleToken")).thenReturn("validName");
-		Mockito.when(UserRepo.findByUsername("validName") ).thenReturn(new User());
+		Mockito.when(userRepo.findByUsername("validName") ).thenReturn(new User());
 		
 		doFilterInternal.invoke(tokenFilter, request, response, mockChain);
 		assertTrue(SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
