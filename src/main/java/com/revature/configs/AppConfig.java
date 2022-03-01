5d98c767d152c46ef1df3749896ee49e15d35270
John-Salguero <John.Salguero@Revature.net>
2022-03-01 14:31:35 -0800

    Login Functionality now communication with Front-End
    * Added a CORS Filter Configuration
    * Changed the default port of the server

--- src/main/java/com/revature/configs/AppConfig.java
+++ src/main/java/com/revature/configs/AppConfig.java
@@ -3,6 +3,11 @@ package com.revature.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
 
 /**
  * This component provides registration password hashing functionality.
@@ -15,5 +20,24 @@ public class AppConfig {
     public BCryptPasswordEncoder encodePassword()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        ArrayList<String> allowedOrigins = new ArrayList<String>();
+        allowedOrigins.add("http://localhost:3000");
+        allowedOrigins.add("https://localhost:3000");
+        config.setAllowedOrigins(allowedOrigins);
+        ArrayList<String> exposedHeaders = new ArrayList<String>();
+        exposedHeaders.add("Authorization");
+        config.setExposedHeaders(exposedHeaders);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
