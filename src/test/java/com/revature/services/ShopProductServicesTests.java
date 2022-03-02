a167ab1185a8244826ca7406d7a9cf50df7110dd
Bobby <bodavis@csumb.edu>
2022-03-02 05:46:13 -0800

    Add ShopProduct mock object

--- src/test/java/com/revature/services/ShopProductServicesTests.java
+++ src/test/java/com/revature/services/ShopProductServicesTests.java
@@ -1,12 +1,17 @@
 package com.revature.services;
 
 import com.revature.dartcart.DartCartApplication;
+import com.revature.models.Category;
+import com.revature.models.Product;
 import com.revature.models.ShopProduct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +22,17 @@ public class ShopProductServicesTests {
 
     @MockBean
     private ShopProductRepository ShopProductRepo;
+
+    final static ShopProduct testShopProduct = new ShopProduct(
+            1,
+            new Product(1,
+                    "testProduct",
+                    "testDescription",
+                    new ArrayList<Category>(Arrays.asList(new Category(1, "testCategory")))),
+            20,
+            50.0,
+            0.0
+    );
 
     @Test
     void getShop_ProductById(int id) {
