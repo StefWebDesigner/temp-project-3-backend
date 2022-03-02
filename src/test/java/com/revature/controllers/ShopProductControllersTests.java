7fc9f1a94db8da00d93871cdf8ec3366af76c389
Bobby <bodavis@csumb.edu>
2022-03-02 05:37:05 -0800

    Add important annotations to ShopProductControllersTests

--- src/test/java/com/revature/controllers/ShopProductControllersTests.java
+++ src/test/java/com/revature/controllers/ShopProductControllersTests.java
@@ -1,29 +1,44 @@
 package com.revature.controllers;
 
+import com.revature.dartcart.DartCartApplication;
 import com.revature.models.Category;
 import com.revature.models.Product;
 import com.revature.models.ShopProduct;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = DartCartApplication.class)
 public class ShopProductControllersTests {
-
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @MockBean
     private ShopProductService sps;
+
+    @BeforeEach
+    void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     final static ShopProduct testShopProduct = new ShopProduct(
             1,
