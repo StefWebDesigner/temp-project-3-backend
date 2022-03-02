69b91774bbf20290413490ca456c2be294a01b27
Bobby <bodavis@csumb.edu>
2022-03-02 05:39:58 -0800

    Add SpringBootTest annotation

--- src/test/java/com/revature/services/ShopProductServicesTests.java
+++ src/test/java/com/revature/services/ShopProductServicesTests.java
@@ -1,15 +1,17 @@
 package com.revature.services;
 
+import com.revature.dartcart.DartCartApplication;
 import com.revature.models.ShopProduct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = DartCartApplication.class)
 public class ShopProductServicesTests {
-
-
     @Autowired
     private ShopProductService ShopProductService;
 
