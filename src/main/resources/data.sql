3b7cc55ad32ad2c8c0268907ed856255d60cc446
Portakin <54554311+Cacealeos@users.noreply.github.com>
2022-03-04 08:27:42 -0800

    change model name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -145,10 +145,10 @@ INSERT INTO Products (product_id, name, description) VALUES
 (1, 'Kelloggs Froot Loops', 'Delicious frooty flava');
 
 
-INSERT INTO Shop_Products (shop_product_id, quantity, price, discount, product_id) VALUES
-(1, 10, 15, 2, 1);
+INSERT INTO Shop_Products (shop_product_id, shop_id, quantity, price, discount, product_id) VALUES
+(1, 1, 10, 15, 2, 1);
 
-INSERT INTO Categories (category_id, name) VALUES (1, 'Food');
+INSERT INTO Categories (category_id, name) VALUES (1, 'Perishable');
 
 INSERT INTO Product_Category (product_id, category_id) VALUES
 (1, 1);
