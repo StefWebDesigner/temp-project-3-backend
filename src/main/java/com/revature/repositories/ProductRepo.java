6791dfce14cd577f34689707498eaed21cd44f18
A.Samad Sadat <sadatabdulsamad@yahoo.com>
2022-03-05 21:21:04 -0500

    feat: add category search for front-end categories drop down

--- src/main/java/com/revature/repositories/ProductRepo.java
+++ src/main/java/com/revature/repositories/ProductRepo.java
@@ -5,7 +5,11 @@ import com.revature.models.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
     public interface ProductRepo extends CrudRepository<Product, Integer> {
+
+    public List<Product> findByCategories(String category);
 
 }
