5a9d5ba6872c0021f3c963bca667dedd35474468
Gurman Singh <gurmans2017@gmail.com>
2022-03-16 17:10:07 -0400

    implemented emailer for sending out password reset information, currently trying to take in username from frontend and collect destination email based on that param

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -4,6 +4,7 @@ import com.revature.models.UserLogin;
 import com.revature.services.AuthService;
 import com.revature.services.UserService;
 import com.revature.utilities.JwtTokenUtil;
+import com.revature.utilities.EmailServiceImpl;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,45 +24,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class AuthController {
-  @Autowired
-  AuthenticationManager authenticationManager;
+	/*
+	 * @Autowired AuthenticationManager authenticationManager;
+	 * 
+	 * @Autowired JwtTokenUtil jwtTokenUtil;
+	 * 
+	 * @Autowired UserService userService;
+	 * 
+	 * @Autowired AuthService authService;
+	 * 
+	 */
+	private final AuthenticationManager authenticationManager;
+	private final JwtTokenUtil jwtTokenUtil;
+	private final UserService userService;
+	private final AuthService authService;
+	private final EmailServiceImpl emailServiceImp;
 
-  @Autowired
-  JwtTokenUtil jwtTokenUtil;
+	@Autowired
+	public AuthController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil,
+			UserService userService, AuthService authService, EmailServiceImpl emailServiceImp) {
+		super();
+		this.authenticationManager = authenticationManager;
+		this.jwtTokenUtil = jwtTokenUtil;
+		this.userService = userService;
+		this.authService = authService;
+		this.emailServiceImp = emailServiceImp;
+	}
 
-  @Autowired
-  UserService userService;
+	@PostMapping("/login")
+	public ResponseEntity<com.revature.models.User> login(@RequestBody UserLogin request) {
+		try {
+			Authentication authenticate = authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 
-  @Autowired
-  AuthService authService;
+			User user = (User) authenticate.getPrincipal();
+			com.revature.models.User retUser = userService
+					.getUserByUsername(user.getUsername().toLowerCase(Locale.ROOT));
+			retUser.setPassword(null);
 
-  @PostMapping("/login")
-  public ResponseEntity<com.revature.models.User> login(
-    @RequestBody UserLogin request
-  ) {
-    try {
-      Authentication authenticate = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(
-          request.getUsername(),
-          request.getPassword()
-        )
-      );
-
-      User user = (User) authenticate.getPrincipal();
-      com.revature.models.User retUser = userService.getUserByUsername(
-        user.getUsername().toLowerCase(Locale.ROOT)
-      );
-      retUser.setPassword(null);
-
-      return ResponseEntity
-        .ok()
-        .header(
-          HttpHeaders.AUTHORIZATION,
-          jwtTokenUtil.generateAccessToken(user)
-        )
-        .body(retUser);
-    } catch (BadCredentialsException ex) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-  }
+			return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, jwtTokenUtil.generateAccessToken(user))
+					.body(retUser);
+		} catch (BadCredentialsException ex) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+	}
+	
+	@GetMapping("/resetpass")
+	public String resetPassword(@RequestBody String username) {
+		com.revature.models.User user = userService.getUserByUsername(username);
+		emailServiceImp.sendSimpleMessage(user.getEmail(), "password reset email test", "didn't implement yet stay tuned :)");
+		return "test";
+	}
 }
