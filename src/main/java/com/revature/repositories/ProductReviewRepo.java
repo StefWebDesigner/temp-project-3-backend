dfd5260a9bfc53973cf6f4b5d1537fe2f5830fd8
Mike Campbell <70548966+maikamp@users.noreply.github.com>
2022-03-17 14:36:20 -0400

    added query for find all by user id, added query for find all by product id, not sure about syntax

--- src/main/java/com/revature/repositories/ProductReviewRepo.java
+++ src/main/java/com/revature/repositories/ProductReviewRepo.java
@@ -2,8 +2,17 @@ package com.revature.repositories;
 
 import com.revature.models.ProductReview;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductReviewRepo extends CrudRepository<ProductReview, Integer> {
+    
+    @Query("SELECT * FROM ProductReview p WHERE p.user_id = :userId")
+    Iterable<ProductReview> findAllByUserId(int userId);
+    
+    @Query("SELECT * FROM ProductReview p WHERE p.product_id = :productId")
+    Iterable<ProductReview> findAllByProductId(int productId);
 
 }
