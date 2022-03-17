e13b6aa955bf4ff4aacfeb5c6a22076d511d3111
Mike Campbell <70548966+maikamp@users.noreply.github.com>
2022-03-17 14:42:13 -0400

    updated query syntax

--- src/main/java/com/revature/repositories/ProductReviewRepo.java
+++ src/main/java/com/revature/repositories/ProductReviewRepo.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductReviewRepo extends CrudRepository<ProductReview, Integer> {
-    
-    @Query("SELECT * FROM ProductReview p WHERE p.user_id = :userId")
+    //using syntax from https://www.baeldung.com/spring-data-jpa-query 5.1 JPQL
+    @Query("SELECT p FROM ProductReview p WHERE p.user_id = ?1")
     Iterable<ProductReview> findAllByUserId(int userId);
     
-    @Query("SELECT * FROM ProductReview p WHERE p.product_id = :productId")
+    @Query("SELECT p FROM ProductReview p WHERE p.product_id = ?1")
     Iterable<ProductReview> findAllByProductId(int productId);
 
 }
