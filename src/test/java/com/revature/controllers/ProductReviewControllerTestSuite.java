b7743a2ade29097aca0aedad4d5c74d3b0ceaff2
Mike Campbell <70548966+maikamp@users.noreply.github.com>
2022-03-29 22:22:21 -0400

    added test coverage for PR get mappings

--- src/test/java/com/revature/controllers/ProductReviewControllerTestSuite.java
+++ src/test/java/com/revature/controllers/ProductReviewControllerTestSuite.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import com.revature.driver.DartCartApplication;
@@ -53,15 +55,7 @@ public class ProductReviewControllerTestSuite {
     // }
 
     @Test
-    public void test_newProductReview_returns200_givenValidRequest() throws Exception {
-        User user = new User();
-        user.setId(1);
-        user.setUsername("username");
-        Product product = new Product();
-        product.setId(1);
-        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
-        Authentication auth = mock(Authentication.class);
-
+    public void test_newProductReview_returns200_givenValidRequest() {
         mockProductReviewService = mock(ProductReviewService.class);
         mockProductService = mock(ProductServiceImpl.class);
         mockUserService = mock(UserServiceImpl.class);
@@ -69,7 +63,13 @@ public class ProductReviewControllerTestSuite {
                 mockProductReviewService,
                 mockUserService,
                 mockProductService);
-
+        User user = new User();
+        user.setId(1);
+        user.setUsername("username");
+        Product product = new Product();
+        product.setId(1);
+        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
+        Authentication auth = mock(Authentication.class);
         when(auth.getName()).thenReturn("username");
         when(mockUserService.getUserByUsername("username")).thenReturn(user);
         when(mockProductService.getProductById(1)).thenReturn(Optional.of(product));
@@ -83,15 +83,7 @@ public class ProductReviewControllerTestSuite {
 
     @Test
     public void test_newProductReview_returns400_givenInvalidRequest() {
-        //TODO throws exception
-        User user = new User();
-        user.setId(1);
-        user.setUsername("username");
-        Product product = new Product();
-        product.setId(1);
-        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
-        Authentication auth = mock(Authentication.class);
-
+        // TODO throws exception
         mockProductReviewService = mock(ProductReviewService.class);
         mockProductService = mock(ProductServiceImpl.class);
         mockUserService = mock(UserServiceImpl.class);
@@ -99,7 +91,13 @@ public class ProductReviewControllerTestSuite {
                 mockProductReviewService,
                 mockUserService,
                 mockProductService);
-
+        User user = new User();
+        user.setId(1);
+        user.setUsername("username");
+        Product product = new Product();
+        product.setId(1);
+        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
+        Authentication auth = mock(Authentication.class);
         when(auth.getName()).thenReturn("username");
         when(mockUserService.getUserByUsername("username")).thenReturn(user);
         when(mockProductService.getProductById(1)).thenReturn(null);
@@ -113,14 +111,6 @@ public class ProductReviewControllerTestSuite {
 
     @Test
     public void test_newProductReview_returns403_givenDuplicateProductReview() {
-        User user = new User();
-        user.setId(1);
-        user.setUsername("username");
-        Product product = new Product();
-        product.setId(1);
-        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
-        Authentication auth = mock(Authentication.class);
-
         mockProductReviewService = mock(ProductReviewService.class);
         mockProductService = mock(ProductServiceImpl.class);
         mockUserService = mock(UserServiceImpl.class);
@@ -128,7 +118,13 @@ public class ProductReviewControllerTestSuite {
                 mockProductReviewService,
                 mockUserService,
                 mockProductService);
-
+        User user = new User();
+        user.setId(1);
+        user.setUsername("username");
+        Product product = new Product();
+        product.setId(1);
+        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
+        Authentication auth = mock(Authentication.class);
         when(auth.getName()).thenReturn("username");
         when(mockUserService.getUserByUsername("username")).thenReturn(user);
         when(mockProductService.getProductById(1)).thenReturn(Optional.of(product));
@@ -142,14 +138,6 @@ public class ProductReviewControllerTestSuite {
 
     @Test
     public void test_newProductReview_returns406_givenProductId() {
-        User user = new User();
-        user.setId(1);
-        user.setUsername("username");
-        Product product = new Product();
-        product.setId(1);
-        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
-        Authentication auth = mock(Authentication.class);
-
         mockProductReviewService = mock(ProductReviewService.class);
         mockProductService = mock(ProductServiceImpl.class);
         mockUserService = mock(UserServiceImpl.class);
@@ -157,7 +145,13 @@ public class ProductReviewControllerTestSuite {
                 mockProductReviewService,
                 mockUserService,
                 mockProductService);
-
+        User user = new User();
+        user.setId(1);
+        user.setUsername("username");
+        Product product = new Product();
+        product.setId(1);
+        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
+        Authentication auth = mock(Authentication.class);
         when(auth.getName()).thenReturn("username");
         when(mockUserService.getUserByUsername("username")).thenReturn(user);
         // when(mockProductService.getProductById(1)).thenReturn(null);
@@ -171,14 +165,6 @@ public class ProductReviewControllerTestSuite {
 
     @Test
     public void test_newProductReview_returns500_onServerError() {
-        User user = new User();
-        user.setId(1);
-        user.setUsername("username");
-        Product product = new Product();
-        product.setId(1);
-        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
-        Authentication auth = mock(Authentication.class);
-
         mockProductReviewService = mock(ProductReviewService.class);
         mockProductService = mock(ProductServiceImpl.class);
         mockUserService = mock(UserServiceImpl.class);
@@ -186,7 +172,13 @@ public class ProductReviewControllerTestSuite {
                 mockProductReviewService,
                 mockUserService,
                 mockProductService);
-
+        User user = new User();
+        user.setId(1);
+        user.setUsername("username");
+        Product product = new Product();
+        product.setId(1);
+        ProductReview pr = new ProductReview(1, "valid", "valid", 3, user, product);
+        Authentication auth = mock(Authentication.class);
         when(auth.getName()).thenReturn("username");
         when(mockUserService.getUserByUsername("username")).thenReturn(user);
         when(mockProductService.getProductById(1)).thenReturn(Optional.of(product));
@@ -198,4 +190,109 @@ public class ProductReviewControllerTestSuite {
         Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
     }
 
+    @Test
+    public void test_getProductReviews_returnsList_givenValidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        List<ProductReview> reviews = new ArrayList<>();
+        when(mockProductReviewService.findAllProductReviews()).thenReturn(reviews);
+
+        ResponseEntity<List> responseEntity = sut.getProductReviews();
+
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void test_getProductReviews_returns400_givenInvalidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        when(mockProductReviewService.findAllProductReviews()).thenThrow(new RuntimeException());
+        
+        ResponseEntity<List> responseEntity = sut.getProductReviews();
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void test_getProductReviewByUser_returnsList_givenValidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        User user = new User();
+        user.setId(1);
+        List<ProductReview> reviews = new ArrayList<>();
+        when(mockUserService.getUserById(1)).thenReturn(Optional.of(user));
+        when(mockProductReviewService.findAllProductReviewsByUser(user)).thenReturn(reviews);
+
+        ResponseEntity<List> responseEntity = sut.getProductReviewByUser(1);
+
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+    @Test
+    public void test_getProductReviewByUser_returns400_givenInvalidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        User user = new User();
+        user.setId(1);
+        when(mockUserService.getUserById(1)).thenReturn(null);
+        //when(mockProductReviewService.findAllProductReviewsByUser(user)).thenThrow(new RuntimeException());
+        ResponseEntity<List> responseEntity = sut.getProductReviewByUser(1);
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+    @Test
+    public void test_getProductReviewByProduct_returnsList_givenValidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        Product product = new Product();
+        product.setId(1);
+        List<ProductReview> reviews = new ArrayList<>();
+        when(mockProductService.getProductById(1)).thenReturn(Optional.of(product));
+        when(mockProductReviewService.findAllProductReviewsByProduct(product)).thenReturn(reviews);
+
+        ResponseEntity<List> responseEntity = sut.getProductReviewByProduct(1);
+
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+    @Test
+    public void test_getProductReviewByProduct_returns400_givenInvalidRequest() {
+        mockProductReviewService = mock(ProductReviewService.class);
+        mockProductService = mock(ProductServiceImpl.class);
+        mockUserService = mock(UserServiceImpl.class);
+        sut = new ProductReviewController(
+                mockProductReviewService,
+                mockUserService,
+                mockProductService);
+        Product product = new Product();
+        product.setId(1);
+        when(mockProductService.getProductById(1)).thenReturn(null);
+        //when(mockProductReviewService.findAllProductReviewsByProduct(product)).thenThrow(new RuntimeException());
+        ResponseEntity<List> responseEntity = sut.getProductReviewByProduct(1);
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
 }
