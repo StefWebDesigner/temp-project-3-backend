8d5e6bb129bde9f8b6216563bed0dffa5dd1160e
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-17 13:02:34 -0400

    added Stomp folders

---
+++ src/main/java/com/revature/controllers/StompController.java
@@ -0,0 +1,26 @@
+package com.revature.controllers;
+
+
+import com.revature.services.StompService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/greeting")
+public class StompController {
+
+
+    private final StompService stompService;
+
+    @Autowired
+    public StompController(StompService stompService) {
+        this.stompService = stompService;
+    }
+
+
+
+    
+}
